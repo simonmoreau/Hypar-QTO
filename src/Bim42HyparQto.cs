229d6bcede8f8750ce39564a7184133a192cc1ae
Simon Moreau <simon@bim42.com>
2019-04-23 22:23:20 +0200

    add ceiling, raised floor, better level height management

--- src/Bim42HyparQto.cs
+++ src/Bim42HyparQto.cs
@@ -16,30 +16,39 @@ namespace Bim42HyparQto
         public double building_lenght;
         public double core_width = 5;
         public double module_lenght = 1.35;
+
+        //Set building lenght
+        public int module_number = 20;
+        //Set level height definition
+        public double level_height = 3.5;
+        public double ceiling_void_height = 0.05;
+        public double ceiling_thickness = 0.05;
+        public double raised_floor_void_height = 0.07;
+        public double raised_floor_thickness = 0.03;
+        public double headspace = 2.7;
+
+
+        //Set structural dimensions
+        public double beam_height = 0.6; //full height, including slab thickness
+        public double slab_height = 0.2;
+        public double column_diameter = 0.5;
+        public double facade_thickness = 0.4;
+
         public Output Execute(Input input)
         {
 
             // Create a model
             Model model = new Model();
             double area = 0;
-
-            //Set building lenght
-            int module_number = 20;
             building_lenght = module_lenght * module_number;
+            level_height = 
+            raised_floor_void_height + 
+            raised_floor_thickness +
+            headspace +
+            ceiling_thickness +
+            ceiling_void_height +
+            beam_height;
 
-            double level_height = 3.5;
-
-            //Set structural dimensions
-            double beam_height = 0.7; //full dimension, including slab thickness
-            double slab_height = 0.2;
-            double column_diameter = 0.5;
-            double facade_thickness = 0.4;
-
-            //Create wall and floor types
-            FloorType slabType = new FloorType("slab", slab_height, null);
-            FloorType raisedFloorype = new FloorType("Raised floor", 0.03, null);
-
-            WallType facadeType = new WallType("façade", facade_thickness, "façade");
 
             Line topAxe = new Line(new Vector3(0, building_width, 0), new Vector3(building_lenght, building_width, 0));
             Line bottomAxe = new Line(new Vector3(0, 0, 0), new Vector3(building_lenght, 0, 0));
@@ -49,62 +58,84 @@ namespace Bim42HyparQto
 
             for (int i = 0; i < grid.Cells().GetLength(0); i++)
             {
-                Vector3[] bottomCell = grid.Cells()[i, 0];
-                Vector3[] topCell = grid.Cells()[i, 0];
-
-                //Create a façade
-                Vector3 facadeOffset = new Vector3(0,facade_thickness/2,0);
-                Line bottomBaseLine = new Line(bottomCell[0]+facadeOffset, bottomCell[3]+facadeOffset);
-                Wall bottomWall = new Wall(bottomBaseLine, facadeType, level_height, BuiltInMaterials.Glass, null, null);
-                model.AddElement(bottomWall);
-
-                Vector3 innerOffset = new Vector3(0,facade_thickness,0);
-                Vector3[] innerCell = new Vector3[] {bottomCell[0] + innerOffset,bottomCell[1],bottomCell[2],bottomCell[3] + innerOffset};
-                //Create a slab
-                Polygon bottomPolygon = new Polygon(innerCell);
-                Floor bottomFloor = new Floor(bottomPolygon, slabType, level_height, BuiltInMaterials.Steel, null, null);
-                model.AddElement(bottomFloor);
-                area = area + bottomFloor.Area();
-
-                //Create a raised floor
-                Floor raisedFloor = new Floor(bottomPolygon, raisedFloorype, 0.13, BuiltInMaterials.Wood, null, null);
-                model.AddElement(raisedFloor);
-
-
+                Vector3[] cell = grid.Cells()[i, 0];
+                Vector3[] topCell = grid.Cells()[i, maxRow];
+                topCell = new Vector3[] { topCell[2], topCell[3], topCell[0], topCell[1] };
 
                 //Create a beam each 3 module
                 Math.DivRem(i, 3, out int remainer);
                 if (remainer == 0)
                 {
-                    //Create columns
-                    Profile circularColumnProfile = new Profile(Polygon.Circle(column_diameter / 2));
-                    double column_height = level_height - beam_height;
-                    Column circularColumn = new Column(innerCell[0] + new Vector3(0, 0.5, 0), column_height, circularColumnProfile, BuiltInMaterials.Steel, null, 0, 0);
-                    model.AddElement(circularColumn);
-
-                    //Create beams
-                    Profile beamProfile = new Profile(Polygon.Rectangle(column_diameter - 0.2, beam_height - slab_height));
-                    Vector3 beamElevation = new Vector3(0, 0, level_height - slab_height - (beam_height - slab_height) / 2);
-                    Line beamLine = new Line(innerCell[0] + beamElevation, innerCell[1] + beamElevation);
-
-
-                    Beam beam = new Beam(beamLine, beamProfile, BuiltInMaterials.Steel);
-                    model.AddElement(beam);
+                    area = CreateModule(model, area, cell, true);
+                    area = CreateModule(model, area, topCell, true);
+                }
+                else
+                {
+                    area = CreateModule(model, area, cell, false);
+                    area = CreateModule(model, area, topCell, false);
                 }
 
-                Polygon topPolygon = new Polygon(grid.Cells()[i, maxRow]);
-                Floor topFloor = new Floor(topPolygon, slabType, 0, null, null, null);
-                model.AddElement(topFloor);
-
-                Line topBaseLine = new Line(grid.Cells()[i, maxRow][1], grid.Cells()[i, maxRow][2]);
-                Wall topWall = new Wall(topBaseLine, facadeType, level_height, BuiltInMaterials.Glass, null, null);
-                model.AddElement(topWall);
-
-                area = area + topFloor.Area();
             }
 
 
             return new Output(model, area); ;
+        }
+
+        public double CreateModule(Model model, double area, Vector3[] cell, bool structural)
+        {
+            //Create wall and floor types
+            FloorType slabType = new FloorType("slab", slab_height, null);
+            FloorType raisedFloorType = new FloorType("Raised floor", raised_floor_thickness, null);
+            FloorType ceillingType = new FloorType("Ceilling", ceiling_thickness,null);
+            WallType facadeType = new WallType("façade", facade_thickness, "façade");
+
+            //Helper vector
+            Vector3 towardInside = (cell[1]-cell[0]).Normalized();
+
+            //Create a façade
+            Vector3 facadeOffset = towardInside*(facade_thickness / 2);
+            Line bottomBaseLine = new Line(cell[0] + facadeOffset, cell[3] + facadeOffset);
+            Wall bottomWall = new Wall(bottomBaseLine, facadeType, level_height, BuiltInMaterials.Glass, null, null);
+            model.AddElement(bottomWall);
+
+            Vector3 innerOffset = towardInside *(facade_thickness);
+            Vector3[] innerCell = new Vector3[] { cell[0] + innerOffset, cell[1], cell[2], cell[3] + innerOffset };
+            //Create a slab
+            Polygon innerPolygon = new Polygon(innerCell);
+            Floor bottomFloor = new Floor(innerPolygon, slabType, level_height, BuiltInMaterials.Steel, null, null);
+            model.AddElement(bottomFloor);
+            area = area + bottomFloor.Area();
+
+            //Create a raised floor
+            Floor raisedFloor = new Floor(innerPolygon, raisedFloorType, raised_floor_thickness + raised_floor_void_height, BuiltInMaterials.Wood, null, null);
+            model.AddElement(raisedFloor);
+
+            //Create a ceilling
+            double ceilingElevation = raised_floor_void_height + raised_floor_thickness + headspace + ceiling_thickness;
+            Floor ceilling = new Floor(innerPolygon, ceillingType, ceilingElevation , BuiltInMaterials.Wood, null, null);
+            model.AddElement(ceilling);
+
+            if (structural)
+            {
+                //Create columns
+                Profile circularColumnProfile = new Profile(Polygon.Circle(column_diameter / 2));
+                double column_height = level_height - beam_height;
+                Vector3 columnOffset = towardInside*0.5;
+                Column circularColumn = new Column(innerCell[0] + columnOffset, column_height, circularColumnProfile, BuiltInMaterials.Steel, null, 0, 0);
+                model.AddElement(circularColumn);
+
+                //Create beams
+                Profile beamProfile = new Profile(Polygon.Rectangle(column_diameter, beam_height - slab_height));
+                Vector3 beamElevation = new Vector3(0, 0, level_height - slab_height - (beam_height - slab_height) / 2);
+                Line beamLine = new Line(innerCell[0] + beamElevation, innerCell[1] + beamElevation);
+
+
+                Beam beam = new Beam(beamLine, beamProfile, BuiltInMaterials.Steel);
+                model.AddElement(beam);
+            }
+
+            return area;
+
         }
 
         public List<Polygon> CreatePolygonsFromLines(List<Outline.Line> lines)
